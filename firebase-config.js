59999653d8ab7178f41fc68122dda96dc1b83c76
SamassaTechnologie <samassatechnologie10@gmail.com>
2026-04-22 06:38:04 +0000

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@
 const FIREBASE_CONFIG = {
   apiKey:            "AIzaSyDxRqnmRraoF4JpHLPAwEmMbLw9qRb2h58",
   authDomain:        "samassa-kayes-79fbb.firebaseapp.com",
-  databaseURL:       "https://samassa-kayes-79fbb-default-rtdb.europe-west1.firebasedatabase",
+  databaseURL:       "https://samassa-kayes-79fbb-default-rtdb.europe-west1.firebasedatabase.app",
   projectId:         "samassa-kayes-79fbb",
   storageBucket:     "samassa-kayes-79fbb.firebasestorage.app",
   messagingSenderId: "704230186166",
